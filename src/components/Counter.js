03aaa8e1339ab86584d962acc30c2567c667e80c
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-08-01 07:19:17 -0700

    Add files via upload

---
+++ src/components/Counter.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import Button from "./Button";
+import Input from "./Input";
+import '../assets/styles/Counter.css';
+
+// Counter, with discount feature, function
+
+function Counter() {
+    const[count, setCount] = useState(0);
+    const[discount, setDiscount] = useState(0);
+    const[name, setName] = useState('Maracuya');
+
+    const price = 1.39;
+
+    // add event for "+" button
+
+    const handlePlus = () => {
+
+        // function myFunction(prevCount) {
+        //      let sum = prevCount + 1;
+        //
+        //      if (sum >=5) {
+        //   setDiscount(20);
+        //}
+        //
+        //      return sum;
+        //}
+
+        // setCount(myFunction);
+
+        setCount ((prevCount) => {
+            let sum = prevCount + 1;
+            if (sum >= 5) {
+                setDiscount(20);
+            }
+            return sum;
+        })
+
+    }
+
+
+// minus event
+
+const handleMinus = () => {
+    setCount((preCount) => {
+        let diff = prevCount -1;
+        if(diff < 5) {
+            setDiscount(0)
+        }
+        return diff;
+    })
+};
+
+
+return (
+    <>
+        <h2>{name}</h2>
+        <Button label="-" onClick={hanldeMinus} />
+
+        <span>{count}</span>
+        <Button label="+" onClick={handlePlus} />
+
+        <h2>{`$ ${price}`}</h2>
+        <h3>{`Discount: ${discount}%`}</h3>
+    </>
+)
+};
